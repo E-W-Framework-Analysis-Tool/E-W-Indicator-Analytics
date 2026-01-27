9c35daad9fe1667c7390c458ebecac23891eb7d4
Shailendra Singh <s.singh@wearedoubleline.com>
2026-01-27 07:07:30 -0600

    feat: add-update first three indicators
    pending: y-o-y for Indicator 2

--- Dashboards.Database/Objects/bi/Views/vw_DimK12CourseStatuses.sql
+++ Dashboards.Database/Objects/bi/Views/vw_DimK12CourseStatuses.sql
@@ -1,4 +1,11 @@
 CREATE VIEW [bi].[vw_DimK12CourseStatuses]
 AS
-	SELECT *
+	SELECT *,
+	CASE
+		WHEN CourseLevelCharacteristicDescription = 'International Baccalaureate course' THEN 'IB'
+		WHEN CourseLevelCharacteristicDescription = 'Dual enrollment' THEN 'Dual Credit'
+		WHEN CourseLevelCharacteristicDescription = 'General course' THEN 'General'
+		WHEN CourseLevelCharacteristicDescription = 'Advanced placement course' THEN 'AP'
+		ELSE 'Other'
+	END AS CourseLevel
 	FROM [reporting].[DimK12CourseStatuses];
