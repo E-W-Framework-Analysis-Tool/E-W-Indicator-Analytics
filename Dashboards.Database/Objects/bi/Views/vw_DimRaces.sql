8fde5598193e2f16c3b4d1e2c725e2094abdb41a
Shailendra Singh <s.singh@wearedoubleline.com>
2026-01-29 11:19:40 -0600

    fix: updated theme

--- Dashboards.Database/Objects/bi/Views/vw_DimRaces.sql
+++ Dashboards.Database/Objects/bi/Views/vw_DimRaces.sql
@@ -1,5 +1,8 @@
 ﻿CREATE VIEW [bi].[vw_DimRaces] AS
 SELECT
 	[DimRaceId]
-  , [RaceDescription] AS [RaceEthnicity]
+  , CASE 
+		WHEN [RaceDescription] = 'Demographic Race Two or More Races' THEN 'Two or More Races'
+		WHEN [RaceDescription] = 'Hispanic' THEN 'Hispanic or Latino'
+	ELSE [RaceDescription] END AS [RaceEthnicity]
 FROM [reporting].[DimRaces];
