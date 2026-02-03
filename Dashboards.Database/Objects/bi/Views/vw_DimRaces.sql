fd9a12a38c8ee4b94204b795a2780c86da19c031
Shailendra Singh <s.singh@wearedoubleline.com>
2026-02-02 21:33:22 -0600

    feat: add trend line in chart 1

--- Dashboards.Database/Objects/bi/Views/vw_DimRaces.sql
+++ Dashboards.Database/Objects/bi/Views/vw_DimRaces.sql
@@ -5,4 +5,5 @@ SELECT
 		WHEN [RaceDescription] = 'Demographic Race Two or More Races' THEN 'Two or More Races'
 		WHEN [RaceDescription] = 'Hispanic' THEN 'Hispanic or Latino'
 	ELSE [RaceDescription] END AS [RaceEthnicity]
-FROM [reporting].[DimRaces];
+FROM [reporting].[DimRaces]
+WHERE [DimRaceId] != -1;
