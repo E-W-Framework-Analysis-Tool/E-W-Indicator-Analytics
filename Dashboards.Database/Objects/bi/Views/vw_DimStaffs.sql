7f77657056144b97415cd44f7fb8d785e8a7602a
Shailendra Singh <s.singh@wearedoubleline.com>
2026-02-26 08:52:32 -0600

    feat: add Career Advising page

--- Dashboards.Database/Objects/bi/Views/vw_DimStaffs.sql
+++ Dashboards.Database/Objects/bi/Views/vw_DimStaffs.sql
@@ -1,8 +1,10 @@
 ﻿CREATE VIEW [bi].[vw_DimStaffs]
-AS
-	SELECT
-		[ds].[DimK12StaffId]
-	FROM
-		[reporting].[DimStaffs]                        AS [ds]
-			LEFT JOIN [reporting].[FactK12StaffCounts] AS [fkcs]
-						  ON [ds].[DimK12StaffId] = [fkcs].[K12StaffId]
+	AS
+		SELECT DISTINCT
+			[ds].[DimK12StaffId]
+		FROM
+			[reporting].[DimStaffs]                        AS [ds]
+				LEFT JOIN [reporting].[FactK12StaffCounts] AS [fkcs]
+							  ON [ds].[DimK12StaffId] = [fkcs].[K12StaffId]
+		WHERE
+			[fkcs].[K12StaffId] IS NOT NULL
