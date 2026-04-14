4b180331a11a4af1592d00eccd960094ca39739a
Shailendra Singh <s.singh@wearedoubleline.com>
2026-04-14 07:41:52 -0500

    feat: add enrollment page

--- Dashboards.Database/Objects/bi/Views/vw_DimCipCodes.sql
+++ Dashboards.Database/Objects/bi/Views/vw_DimCipCodes.sql
@@ -1,4 +1,20 @@
 ﻿CREATE VIEW [bi].[vw_DimCipCodes]
 AS
-	SELECT *
-	FROM [reporting].[DimCipCodes]
+	WITH
+		[base] AS
+			(SELECT
+				 [DimCipCodeId]
+			   , CAST(CAST(IIF([CipCode] = 'MISSING', '-1.0', [CipCode]) AS DECIMAL) AS SMALLINT) AS [CipCode]
+			 FROM [reporting].[DimCipCodes])
+	SELECT
+		[DimCipCodeId]
+	  , CASE
+			WHEN [CipCode] IN (11, 14, 15, 26, 27, 30, 40, 41)
+				THEN 'STEM'
+			WHEN [CipCode] IN (52, 62)
+				THEN 'Business'
+			WHEN [CipCode] IN (5, 16, 23, 24, 38, 39, 45, 50, 54)
+				THEN 'Liberal Arts & Humanities'
+			ELSE 'Other'
+		END AS [FieldOfStudy]
+	FROM [base]
--- Dashboards.Database/Objects/bi/Views/vw_DimPsAcademicAwardStatuses.sql
+++ Dashboards.Database/Objects/bi/Views/vw_DimPsAcademicAwardStatuses.sql
@@ -1,4 +1,26 @@
 ﻿CREATE VIEW [bi].[vw_DimPsAcademicAwardStatuses]
 AS
-	SELECT *
+	SELECT
+		[DimPsAcademicAwardStatusId]
+	  , [PescAwardLevelTypeDescription]
+	  , CASE
+			WHEN [PescAwardLevelTypeDescription] LIKE '%Doctor%'
+				THEN 'Doctoral Degree'
+			WHEN [PescAwardLevelTypeDescription] LIKE '%Master%'
+				THEN 'Master''s Degree'
+			WHEN [PescAwardLevelTypeDescription] LIKE '%Post%'
+				THEN 'Certificate'
+			WHEN [PescAwardLevelTypeDescription] LIKE '%Certificate%'
+				THEN 'Certificate'
+			WHEN [PescAwardLevelTypeDescription] LIKE '%Baccalaur%'
+				THEN 'Bachelor''s Degree'
+			WHEN [PescAwardLevelTypeDescription] LIKE '%Bachelor%'
+				THEN 'Bachelor''s Degree'
+			WHEN [PescAwardLevelTypeDescription] LIKE '%Diploma%'
+				THEN 'Certificate'
+			WHEN [PescAwardLevelTypeDescription] LIKE '%Associate%'
+				THEN 'Associate''s Degree'
+			ELSE 'Other'
+		END AS [CredentialType]
+	  , [ProfessionalOrTechnicalCredentialConferredDescription]
 	FROM [reporting].[DimPsAcademicAwardStatuses]
