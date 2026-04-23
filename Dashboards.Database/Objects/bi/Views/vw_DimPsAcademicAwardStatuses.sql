096a4bb954498609dc2a9c7c0530ccf566f09e18
Shailendra Singh <s.singh@wearedoubleline.com>
2026-04-23 08:43:27 -0500

    feat: fixed page 2

--- Dashboards.Database/Objects/bi/Views/vw_DimPsAcademicAwardStatuses.sql
+++ Dashboards.Database/Objects/bi/Views/vw_DimPsAcademicAwardStatuses.sql
@@ -1,9 +1,18 @@
 ﻿CREATE VIEW [bi].[vw_DimPsAcademicAwardStatuses]
 AS
+	WITH [DimPsAcademicAwardStatuses] AS (
 	SELECT
 		[DimPsAcademicAwardStatusId]
-	  , [PescAwardLevelTypeDescription]
-	  , CASE
+	, [PescAwardLevelTypeDescription]
+	, CASE
+			WHEN [DimPsAcademicAwardStatusId] = 344
+				THEN 'Certificate'
+			WHEN [DimPsAcademicAwardStatusId] = 334
+				THEN 'Bachelor''s Degree'
+			WHEN [DimPsAcademicAwardStatusId] = 354
+				THEN 'Master''s Degree'
+			WHEN [DimPsAcademicAwardStatusId] IN (342, 331, 328)
+				THEN 'Doctoral Degree'
 			WHEN [PescAwardLevelTypeDescription] LIKE '%Doctor%'
 				THEN 'Doctoral Degree'
 			WHEN [PescAwardLevelTypeDescription] LIKE '%Master%'
@@ -22,5 +31,23 @@ AS
 				THEN 'Associate''s Degree'
 			ELSE 'Other'
 		END AS [CredentialType]
-	  , [ProfessionalOrTechnicalCredentialConferredDescription]
-	FROM [reporting].[DimPsAcademicAwardStatuses]
+	, [ProfessionalOrTechnicalCredentialConferredDescription]
+	FROM [reporting].[DimPsAcademicAwardStatuses])
+	SELECT
+		*
+	  , CASE
+			WHEN [CredentialType] = 'Certificate'
+				THEN 1
+			WHEN [CredentialType] = 'Associate''s Degree'
+				THEN 2
+			WHEN [CredentialType] = 'Bachelor''s Degree'
+				THEN 4
+			WHEN [CredentialType] = 'Master''s Degree'
+				THEN 2
+			WHEN [CredentialType] = 'Doctoral Degree'
+				THEN 5
+			ELSE 3
+		END AS [IntendedYears]	
+	FROM [DimPsAcademicAwardStatuses];
+
+GO
